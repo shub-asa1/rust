9eb2369920ded0d129d8f47321e66b6315517bc2
shubham.asati2626@gmail.com <webdeveloper@netmobix.com>
2019-11-28 00:17:56 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/// This is my function for rust documentation
+/// You can embed images in docs tooo....
+/// Sorry for function names :)
+/// ![Cats](https://homepages.cae.wisc.edu/~ece533/images/cat.png)  
+pub fn dummy(){
+    println!("this is dummy function");
+}
+
+
+
+/// Another function for documentation
+
+pub fn tummy(){
+    println!("this is tummy function");
+}
